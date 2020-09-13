63fcf9cb129bade1cb2c8a68e9d7366c6cb3c12b
oluSammy <olumorinsammy@gmail.com>
2020-09-12 19:58:53 -0700

    ,,,

---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from './../context/globalState.context';
+import Transaction from './Transaction';
+
+
+const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+    return (
+        <div>
+            <h3>History</h3>
+            <ul id="list" className="list">
+                {
+                    transactions.map(transaction => 
+                        <Transaction transaction={transaction} />
+                    )
+                }
+                
+            </ul>
+        </div>
+    )
+}
+
+export default TransactionList;
